9a21873386c3b4aaa52923125ae261d9ca67a559
Eloy Ramirez <clustersnake45@gmail.com>
2025-12-31 15:19:05 -0400

    feat: Setup dbcontext to translate entities to tables

---
+++ backend/Todo.Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+
+namespace Todo.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<PriorityEntity> Priorities { get; set; }
+    public DbSet<TaskEntity> Tasks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        // Configurar la relación entre TaskEntity con UserEntity y PriorityEntity 
+
+        modelBuilder.Entity<TaskEntity>()
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tasks)
+            .HasForeignKey(t => t.UserId);
+
+        
+        modelBuilder.Entity<TaskEntity>()
+            .HasOne(t => t.Priority)
+            .WithMany(p => p.Tasks)
+            .HasForeignKey(t => t.PriorityId);
+    }
+}
